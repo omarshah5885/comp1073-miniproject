babe1efe09e929c1895db6551902678b22e13425
omarshah5885 <umarshah0@gmail.com>
2016-02-11 18:23:17 -0500

    JS strings functionality added

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,26 @@
 //iffy 
 (function () {
-	document.body.style.backgroundImage = "url('../content/images/moon-shine.jpg')";
-	/*document.body.style.backgroundColor = "#efefef";*/
-		
+	/*document.body.style.backgroundImage = "url('../content/images/moon-shine.jpg')";
+	document.body.style.backgroundColor = "#efefef";*/
+	
+	var aboutMeTitle = document.getElementById("aboutMeTitle");
+	aboutMeTitle.innerHTML = "About Me";
+	
+	var aboutMe = document.getElementById("aboutMe");
+	aboutMe.innerHTML = "Hello, my name is Omar Shah and I am an ambitious developer that wants to be a professional in the field of marketing with emphasis on digital tools and business intelligence. I am currently enrolled at Georgian College's Interactive Web Design and Development program and am seeking a CO-OP for summer 2016 with a firm that has a strong leadership and work culture.";
+	
+	var project1 = document.getElementById("project1");
+	project1.innerHTML = "This is a website I created for a product before the popularization of the internet. I chose to do it on the Nintendo NES game console. I used no framework for this project and all the layout was performed manually";
+
+	var project2 = document.getElementById("project2");
+	project2.innerHTML = "I created an interactive JavaScript through a complex set of decision structures. The game is based on randomization and each choice has a different set of outcomes.";
+	
+	var project3 = document.getElementById("project3");
+	project3.innerHTML = "This is my first PHP coded assignment which links back to few databases I created using SQL. It is a list of NBA players with their stats and any user can add whoever they like and get an email sent back to them confirming the record. Every input is validated.";
+	
+	var connect = document.getElementById("connect");
+	connect.innerHTML = "Connect With Me";
+	
+	var connectBlurb = document.getElementById("connectBlurb");
+	connectBlurb.innerHTML = "If you're interested with how I can help your organization have a better digital footprint, please feel free to contact me.";
 })();
